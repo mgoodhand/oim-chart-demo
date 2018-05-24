fb424cab2ba07a6bb53582c7ab1dc24ada80bbe5
Mark Goodhand <mrg@corefiling.com>
2018-05-24 14:10:29 +0100

    Drop-down for selecting concept.

--- src/oim-chart.js
+++ src/oim-chart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Plot from 'react-plotly.js';
 import PropTypes from 'prop-types';
 import { Set } from 'immutable';
@@ -14,7 +14,9 @@ export default class OIMChart extends Component {
   }
 
   state = {
-    doc: null
+    doc: null,
+    concept: this.props.concept,
+    concepts: []
   }
 
   componentDidMount() {
@@ -25,16 +27,19 @@ export default class OIMChart extends Component {
     fetch(this.props.src)
       .then(r => r.json())
       .then(data => {
-      this.setState({ doc: data })
+      this.setState({ 
+        doc: data,
+        concepts: OIMChart.concepts(data.facts)
+      })
       console.log(this.state.doc)
       this.facts()
       this.breakdowns()
     })
   }
 
-  concepts() {
-    const concepts = this.state.doc.facts
-       .map(f => f.aspects["xbrl:concept"])
+  static concepts(facts) {
+    const concepts = Set(facts
+       .map(f => f.aspects["xbrl:concept"])).sort()
     console.log("Concepts: ", concepts)
     return concepts
   }
@@ -63,8 +68,20 @@ export default class OIMChart extends Component {
     return breakdowns
   }
 
+  conceptInputChange = e => {
+    this.setState({ concept: e.target.value })
+  }
+
   render() {
     return (
+        <Fragment>
+        <label>Concept:
+        <select onChange={this.conceptInputChange}>
+          { this.state.concepts.map(c => <option value={c} selected={c===this.state.concept}>{c}</option>) }
+        </select>
+        </label>
+        <h2>{this.state.concept}</h2>
+        <div>
         <Plot
         data={[
           {
@@ -88,6 +105,8 @@ export default class OIMChart extends Component {
         layout={{width: this.width, height: this.height, title: this.title}}
         config={{editable: false,  displayModeBar: false}}
       />
+      </div>
+      </Fragment>
     );
   }
 }
